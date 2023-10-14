a369a86faf0862a3efde3f6b87d03d588843898c
StefanRCitiriga <73283856+StefanRCitiriga@users.noreply.github.com>
2023-10-14 17:11:05 +0300

    Added portals and wrappers

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ export default function App() {
   };
 
   return (
-    <div>
+    <>
       <AddUser onAddUser={onAddHandler} />
       <UserList users={usersList} />
-    </div>
+    </>
   );
 }
